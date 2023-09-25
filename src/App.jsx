aab0a9f326b4b7d16bfe27f739900c58b56d0c16
Rami_Kot <ramikr63@gmail.com>
2023-09-26 00:56:31 +0300

    Final

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Carousel from './Carousel'
+import SlickCarousel from './SlickCarousel'
+
+const App = () => {
+  return (
+    <main>
+      {/* USE CSS */}
+      <Carousel />
+      {/* USE react package */}
+      <SlickCarousel />
+    </main>
+  )
+}
+export default App
